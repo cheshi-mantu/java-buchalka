0dc4e6d13c5247a3e6a406c2befc507decae2675
Egor Ivanov <cheshi.mantu@pm.me>
2021-07-22 20:59:33 +0300

    finalised hello world

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,14 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello madafakaz!");
+        System.out.println("Hello madafakaz!"); // huh
+
+        int myVeryFirstNumber = 5 + 10; // declaration statement
+        int mySecondInt = 10;
+        int myThirdNumber = 11;
+
+        int myVeryFirstTotal = myVeryFirstNumber + mySecondInt + myThirdNumber;
+
+        System.out.println("Grand total:  " + myVeryFirstTotal);
     }
 }
