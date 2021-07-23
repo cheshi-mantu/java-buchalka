604aabd2fdd64232889eb31259c8be49bc887752
Egor Ivanov <cheshi.mantu@pm.me>
2021-07-23 19:21:44 +0300

    byte and short casting

--- src/com/github/cheshi_mantu/PrimitivesNumbers.java
+++ src/com/github/cheshi_mantu/PrimitivesNumbers.java
@@ -13,7 +13,9 @@ public class PrimitivesNumbers {
 
         System.out.println("Error will be here : " + (myMaxInt + 1));
 
-        int mytMaxINteger = 2_147_483_647;
+        int mytMaxInteger = 2_147_483_647 ;
+
+        System.out.println("My max integer: " + mytMaxInteger);
 
         byte myMinByte = Byte.MIN_VALUE;
         byte myMaxByte = Byte.MAX_VALUE;
@@ -31,9 +33,26 @@ public class PrimitivesNumbers {
         long myMinLong = Long.MIN_VALUE;
         long myMaxLong = Long.MAX_VALUE;
 
+        long myLongLongValue = 100;
+        long myLongLongLiteralValue = 2_147_483_647_123L;
+
+        System.out.println("My long literal value: " + myLongLongLiteralValue);
+        System.out.println("My long value: " + myLongLongValue);
+
         System.out.println("Min for Long: " + myMinLong);
         System.out.println("Max for Long: " + myMaxLong);
 
+        //casting example
+        int myTotal = (myMinInt / 2); // works fine
+
+        byte myNewByte = (byte) (myMinByte / 2);
+
+        System.out.println("myNewByte casted to byte: " + myNewByte);
+
+
+        short myNewShort = (short) (myMinShort / 2);
+
+        System.out.println("myNewShort casted to short: " + myNewShort);
 
 
 
