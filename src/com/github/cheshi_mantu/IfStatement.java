40e23b8d5cdb100d811642fffcd3143ded4a3eb6
Egor Ivanov <cheshi.mantu@pm.me>
2021-07-23 22:43:07 +0300

    if statement done

--- src/com/github/cheshi_mantu/IfStatement.java
+++ src/com/github/cheshi_mantu/IfStatement.java
@@ -2,6 +2,10 @@ package com.github.cheshi_mantu;
 
 public class IfStatement {
     public static void main(String[] args) {
-
+        boolean isAlien = false; // assignment
+        if (isAlien == false) { // evaluation operator
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
     }
 }
