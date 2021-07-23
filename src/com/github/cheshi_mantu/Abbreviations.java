65cd00ecf1185a68551fbd18420e8c97ff82205f
Egor Ivanov <cheshi.mantu@pm.me>
2021-07-23 22:35:36 +0300

    abbreviations done

--- src/com/github/cheshi_mantu/Abbreviations.java
+++ src/com/github/cheshi_mantu/Abbreviations.java
@@ -2,6 +2,28 @@ package com.github.cheshi_mantu;
 
 public class Abbreviations {
     public static void main(String[] args) {
+        int result = 1;
+        System.out.println("Initial value of result: " + result);
+
+        result++;
+        System.out.println("Result of 'result++': " + result);
+
+        result--;
+        System.out.println("Result of 'result--': " + result);
+
+        result += 2;
+        System.out.println("Result of 'result += 2': " + result);
+
+        result -= 2;
+        System.out.println("Result of 'result -= 3': " + result);
+
+        result *= 20;
+        System.out.println("Result of 'result *= 2': " + result);
+
+        result /= 2;
+        System.out.println("Result of 'result /= 2': " + result);
+
+
 
     }
 }
