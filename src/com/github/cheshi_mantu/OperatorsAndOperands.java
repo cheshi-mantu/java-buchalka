ee6aee5999fb0641314aa9db1fdc1b632c6317a8
Egor Ivanov <cheshi.mantu@pm.me>
2021-07-23 22:22:17 +0300

    Operators and operands

---
+++ src/com/github/cheshi_mantu/OperatorsAndOperands.java
@@ -0,0 +1,14 @@
+package com.github.cheshi_mantu;
+
+public class OperatorsAndOperands {
+    public static void main(String[] args) {
+        String myString = "Kinda string";
+        double myDouble = 2.19d;
+        String resultString = myString + myDouble;
+        // + is the operator
+        // myString is the first Operand
+        // myDounle is the second Operand
+
+
+    }
+}
