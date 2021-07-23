53b0266d6fc689d2f5920c777cc055e9eff2d871
Egor Ivanov <cheshi.mantu@pm.me>
2021-07-23 21:33:14 +0300

    strings examples

--- src/com/github/cheshi_mantu/StringClass.java
+++ src/com/github/cheshi_mantu/StringClass.java
@@ -2,6 +2,24 @@ package com.github.cheshi_mantu;
 
 public class StringClass {
     public static void main(String[] args) {
+    String myString = "Quite short string";
+
+        System.out.println("My stirng is: " + myString);
+
+        myString = myString + ", and this is more.";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + " \u00A9 2019";
+        System.out.println("myString is equal to " + myString);
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println(numberString);
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("LastString is equal to " + lastString);
+        double doubleNumber = 120.47d;
+        lastString = lastString + doubleNumber;
+        System.out.println("LastString is equal to " + lastString);
 
     }
 }
