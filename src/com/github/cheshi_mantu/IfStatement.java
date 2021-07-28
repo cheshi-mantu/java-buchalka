d53d9a37a7a48b4c9109b6722c41dabe045afe38
Egor Ivanov <cheshi.mantu@pm.me>
2021-07-28 19:21:02 +0300

    AND (&&) and OR (||)

--- src/com/github/cheshi_mantu/IfStatement.java
+++ src/com/github/cheshi_mantu/IfStatement.java
@@ -7,5 +7,23 @@ public class IfStatement {
             System.out.println("It is not an alien!");
             System.out.println("And I am scared of aliens");
         }
+
+        int topScore = 80;
+
+        if (topScore >= 80) {
+            System.out.println(" You've got the top score");
+        }
+        // logical AND
+        int secondTopScore = 81;
+        if ((topScore > secondTopScore) && (topScore <= 100)) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Top score is in the right range");
+        }
+
+
+
     }
 }
