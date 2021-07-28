d2ab1d95097bc0441a38df7b797994be177521b3
Egor Ivanov <cheshi.mantu@pm.me>
2021-07-28 19:31:17 +0300

    ternary operator

---
+++ src/com/github/cheshi_mantu/TernaryOperator.java
@@ -0,0 +1,13 @@
+package com.github.cheshi_mantu;
+
+public class TernaryOperator {
+
+    public static void main(String[] args) {
+
+        boolean isCar = false;
+
+        boolean wasCar = isCar ? true : false;
+
+        System.out.println("wasCar is " + wasCar);
+    }
+}
