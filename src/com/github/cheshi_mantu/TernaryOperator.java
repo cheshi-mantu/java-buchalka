644d3efe95b486e1af49602ad14e0a3f0d211faf
Egor Ivanov <cheshi.mantu@pm.me>
2021-07-28 19:50:32 +0300

    "challenge"

--- src/com/github/cheshi_mantu/TernaryOperator.java
+++ src/com/github/cheshi_mantu/TernaryOperator.java
@@ -9,5 +9,28 @@ public class TernaryOperator {
         boolean wasCar = isCar ? true : false;
 
         System.out.println("wasCar is " + wasCar);
+
+        // challenge here
+
+        double firstDoubleVar = 20.0d;
+        double secondDoubleVar = 80.00d;
+
+        double firstResult = 100 * (firstDoubleVar + secondDoubleVar);
+
+        double remainder = firstResult % 40.0d;
+
+        boolean isRemainderZero = remainder == 0 ? true : false;
+
+        System.out.println("is the remainder a zero? - " + isRemainderZero + " " + remainder);
+
+        if (isRemainderZero) {
+            System.out.println("The remainder is zero " + remainder);
+        }
+        else {
+            System.out.println("The remainder is something more than zero: " + remainder);
+        }
+
+
     }
+
 }
