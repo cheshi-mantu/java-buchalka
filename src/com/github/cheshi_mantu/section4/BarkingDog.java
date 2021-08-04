fd857eb272a158e243b12c9e202b73f3c22ece3d
egorivanov <egor.ivanov@pm.me>
2021-08-04 09:54:00 +0300

    BarkingDog skeleton

--- src/com/github/cheshi_mantu/section4/BarkingDog.java
+++ src/com/github/cheshi_mantu/section4/BarkingDog.java
@@ -1,4 +1,14 @@
 package com.github.cheshi_mantu.section4;
 
 public class BarkingDog {
+    public static void main(String[] args) {
+
+    }
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+        if (hourOfDay >=0 && hourOfDay <= 23) {
+        return true;
+        } else {
+         return false;
+        }
+    }
 }
