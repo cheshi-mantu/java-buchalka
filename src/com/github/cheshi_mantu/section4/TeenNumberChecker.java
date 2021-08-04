9627ef6036c24e3c7e33ddd30519f2e3e536b70c
Egor Ivanov <cheshi.mantu@pm.me>
2021-08-04 21:36:24 +0300

    TeenNumberChecker done

--- src/com/github/cheshi_mantu/section4/TeenNumberChecker.java
+++ src/com/github/cheshi_mantu/section4/TeenNumberChecker.java
@@ -1,4 +1,34 @@
 package com.github.cheshi_mantu.section4;
 
 public class TeenNumberChecker {
+    public static void main(String[] args) {
+        System.out.println(hasTeen(9, 99, 19));
+        System.out.println(hasTeen(23, 15, 42));
+        System.out.println(hasTeen(22, 23, 34));
+    }
+
+    public static boolean hasTeen(int one, int two, int three) {
+        if (isTeen(one)) {
+            return true;
+        } else if(isTeen(two)) {
+            return true;
+        } else if(isTeen(three)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+    public static boolean isTeen(int someNumber){
+        int rangeLow = 13;
+        int rangeHigh = 19;
+
+        if ((someNumber >= rangeLow) && (someNumber <= rangeHigh)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
 }
