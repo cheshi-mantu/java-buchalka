86cca63b061bd77a28081c23ba443e914fb0a0ea
Egor Ivanov <cheshi.mantu@pm.me>
2021-08-04 21:15:24 +0300

    LeapYear finished

--- src/com/github/cheshi_mantu/section4/LeapYear.java
+++ src/com/github/cheshi_mantu/section4/LeapYear.java
@@ -1,4 +1,28 @@
 package com.github.cheshi_mantu.section4;
 
 public class LeapYear {
+    public static void main(String[] args) {
+
+        System.out.println(isLeapYear(1800));
+        System.out.println(isLeapYear(1978));
+        System.out.println(isLeapYear(2004));
+        System.out.println(isLeapYear(1924));
+        System.out.println(isLeapYear(2008));
+
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999) {
+            return false;
+        } else {
+            if ((year % 4 == 0) && (year % 100 > 0)) {
+                return true;
+            } else if ((year % 4 == 0) && (year % 100 == 0) && (year % 400 == 0)) {
+                return true;
+            } else {
+                return false;
+            }
+
+        }
+    }
 }
