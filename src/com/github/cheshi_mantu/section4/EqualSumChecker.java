fd37e34db542ee498042884e69032d1aa9d0f94d
egorivanov <egor.ivanov@pm.me>
2021-08-07 20:54:33 +0300

    EqualSumChecker done

--- src/com/github/cheshi_mantu/section4/EqualSumChecker.java
+++ src/com/github/cheshi_mantu/section4/EqualSumChecker.java
@@ -1,4 +1,19 @@
 package com.github.cheshi_mantu.section4;
 
 public class EqualSumChecker {
+    public static void main(String[] args) {
+        System.out.println(hasEqualSum(1,1,2));
+        System.out.println(hasEqualSum(2,-2,0));
+        System.out.println(hasEqualSum(1,2,3));
+        System.out.println(hasEqualSum(2,2,5));
+
+    }
+    public static boolean hasEqualSum(int one, int two, int expectedResult){
+        if (one + two == expectedResult) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
