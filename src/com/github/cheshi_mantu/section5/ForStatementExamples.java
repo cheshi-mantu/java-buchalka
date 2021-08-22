9d7c39a33304373476e720dd859c833b0a944e2b
egorivanov <egor.ivanov@pm.me>
2021-08-22 10:59:13 +0300

    looping for a prime number added

--- src/com/github/cheshi_mantu/section5/ForStatementExamples.java
+++ src/com/github/cheshi_mantu/section5/ForStatementExamples.java
@@ -2,13 +2,36 @@ package com.github.cheshi_mantu.section5;
 
 public class ForStatementExamples {
     public static void main(String[] args) {
-        double amountOfMoney = 10000;
-        for (int interestRate = 2; interestRate <= 5; interestRate++){
-            System.out.println("Interest for " +  amountOfMoney + " and interest rate " + interestRate + " is " + calculateInterestAmount(amountOfMoney, interestRate));
+        double amountOfMoney = 10001;
+        for (int interestRate = 2; interestRate <= 9; interestRate++){
+            System.out.println("Interest for " +  amountOfMoney + " and interest rate " + interestRate + " is " + String.format("%.2f",calculateInterestAmount(amountOfMoney, interestRate)));
+        }
+
+
+        for (int i = 2; i <= 200; i++) {
+            if (isPrime(i)) {
+                System.out.println(i + " is a prime number!");
+            }
         }
      }
     public static double calculateInterestAmount(double amount, double interestRate) {
         return(amount * (interestRate)/100);
 
     }
+    public static boolean isPrime(int n) {
+
+        if(n == 1) {
+            return false;
+        }
+
+        for(int i=2; i <= n/2; i++) {
+//            System.out.println("Looping " +i);
+            if(n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 }
