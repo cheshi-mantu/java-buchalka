5ee92042752b6bb58235f560efc753ed948ad9e8
Egor Ivanov <cheshi.mantu@pm.me>
2021-08-25 20:33:02 +0300

    2 ways to use while

--- src/com/github/cheshi_mantu/section5/WhileAndDoWhile.java
+++ src/com/github/cheshi_mantu/section5/WhileAndDoWhile.java
@@ -2,6 +2,20 @@ package com.github.cheshi_mantu.section5;
 
 public class WhileAndDoWhile {
     public static void main(String[] args) {
+        int count = 0;
+        while(count != 5) {
+            System.out.println("Count = " + count);
+            count++;
+        }
+
+        count = 1;
+        while (true) {
+            System.out.println("Count = " + count);
+            count++;
+            if (count == 6) {
+                break;
+            }
+        }
 
     }
 }
