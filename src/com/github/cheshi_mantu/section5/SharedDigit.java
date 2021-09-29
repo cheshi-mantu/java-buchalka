601cb08295474558bd786074be7452012abee63a
egorivanov <egor.ivanov@pm.me>
2021-09-29 09:53:36 +0300

    done shared digit

--- src/com/github/cheshi_mantu/section5/SharedDigit.java
+++ src/com/github/cheshi_mantu/section5/SharedDigit.java
@@ -2,18 +2,29 @@ package com.github.cheshi_mantu.section5;
 
 public class SharedDigit {
     public static void main(String[] args) {
-        System.out.println(hasSharedDigit(9,12));
-        System.out.println(hasSharedDigit(19,100));
-        System.out.println(hasSharedDigit(22,44));
+        System.out.println(hasSharedDigit(9, 12));
+        System.out.println(hasSharedDigit(19, 999));
+
+
+        System.out.println(hasSharedDigit(22, 44));
+
+        System.out.println(hasSharedDigit(11, 51));
+
+
+        System.out.println(hasSharedDigit(22, 21));
+        System.out.println(hasSharedDigit(21, 12));
 
     }
-    public static boolean hasSharedDigit (int number1, int number2){
-        if (((number1 < 10) || (number1 > 99) ) || ((number2 < 10) || (number2 > 99) )){
+
+    public static boolean hasSharedDigit(int number1, int number2) {
+        if ((number1 < 10) || (number1 > 99) || (number2 < 10) || (number2 > 99)) {
             return false;
         } else {
-            return true;
+            if ((number1 / 10 == number2 / 10) || (number1 / 10 == number2 % 10) || (number1 % 10 == number2 / 10) || (number1 % 10 == number2 % 10)) {
+                return true;
+            } else {
+                return false;
+            }
         }
-
-
     }
 }
