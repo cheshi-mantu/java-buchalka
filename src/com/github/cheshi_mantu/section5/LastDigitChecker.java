594eff93b1923c9f2cebd516388e8d3a31345710
egorivanov <egor.ivanov@pm.me>
2021-09-29 09:58:04 +0300

    initial commit for hasSameLastDigit

---
+++ src/com/github/cheshi_mantu/section5/LastDigitChecker.java
@@ -0,0 +1,13 @@
+package com.github.cheshi_mantu.section5;
+
+public class LastDigitChecker {
+    public static void main(String[] args) {
+
+    }
+    public static boolean hasSameLastDigit(int number1, int number2, int number3){
+
+        return false;
+
+    }
+
+}
